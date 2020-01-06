6d976b08a9f1cf532dce83ad3e034efa6a55b8e3
edijavi <edilata@msn.com>
2020-01-06 16:45:15 +0100

    Added functions to save, delete and make public the images at firestores storage, also the functions keeptDiskSpaceFree was changed to delete just the oldest file

--- cameraTest.py
+++ cameraTest.py
@@ -1,8 +1,7 @@
 import os
 from datetime import datetime
 import picamera
-from google.cloud import storage
-from firebase import firebase
+import saveCloud
 
 picturesURI = "/home/pi/AppPyCharm/Pictures/"
 
@@ -29,10 +28,4 @@ def getTime():
 currentTime = getTime()
 pictureName, filePath = captureImage(currentTime, picturesURI)
 print("%s %s" % (filePath, pictureName))
-
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/pi/AppPyCharm/iotmotionsensor-e737c-firebase-adminsdk-byfm4-2183495c4a.json"
-firebase = firebase.FirebaseApplication('https://iotmotionsensor-e737c.firebaseio.com')
-client = storage.Client()
-bucket = client.get_bucket('iotmotionsensor-e737c.appspot.com')
-imageBlob = bucket.blob("images/%s" % pictureName)
-imageBlob.upload_from_filename(filePath)
+saveCloud.saveToFirebase(pictureName, filePath)
